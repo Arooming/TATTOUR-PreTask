b8c6f30460d603e9e6bf6d8fe1bf48164094b3ca
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-07-03 04:40:08 +0900

    chore: 주석 추가

--- login-test/src/component/KakaoCallback.tsx
+++ login-test/src/component/KakaoCallback.tsx
@@ -18,6 +18,7 @@ function KakaoCallback() {
 
   useEffect(() => {
     if (code) {
+      // 토큰을 받기 위한 코드 (access_token(6시간), refresh_token(2달) 받아올 수 있음)
       axios
         .post(
           `https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&code=${code}`,
@@ -31,12 +32,16 @@ function KakaoCallback() {
         .then((res: resInfo) => {
           console.log(res);
           const { access_token } = res.data;
+          // 유저 개인정보 받아오기 위한 호출 코드
           axios
             .post(
               `https://kapi.kakao.com/v2/user/me`,
+              // post body (post 통신 시, 데이터가 없다면 빈 객체로 보냄)
               {},
+              // request headers
               {
                 headers: {
+                  // Authorization: 사용자 인증 수단, 엑세스 토큰 값
                   Authorization: `Bearer ${access_token}`,
                   "Content-type":
                     "application/x-www-form-urlencoded;charset=utf-8",
@@ -44,6 +49,9 @@ function KakaoCallback() {
               }
             )
             .then((res: resInfo) => {
+              // 유저 id도 발급해줌
+              console.log(res);
+              // 유저 닉네임 가져오기
               console.log(res.data.kakao_account.profile);
             });
         })
