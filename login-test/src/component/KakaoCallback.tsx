9a6584d538ed4b7c89cf4bb91ee0f8f489dcab91
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-07-03 04:27:27 +0900

    feat: 로그인 유저 정보 가져오는 기능 구현

--- login-test/src/component/KakaoCallback.tsx
+++ login-test/src/component/KakaoCallback.tsx
@@ -1,12 +1,55 @@
 import { useEffect } from "react";
+import axios from "axios";
+import { CLIENT_ID, REDIRECT_URI } from "../constant/OAuth";
+
+interface resInfo {
+  data: {
+    kakao_account: {
+      profile: object;
+    };
+    access_token: object;
+  };
+}
 
 function KakaoCallback() {
   // code: 카카오로부터 받은 인가코드
   const code = new URL(window.location.href).searchParams.get("code");
+  const grantType = "authorization_code";
 
   useEffect(() => {
     if (code) {
-      console.log(code);
+      axios
+        .post(
+          `https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&code=${code}`,
+          {},
+          {
+            headers: {
+              "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+            },
+          }
+        )
+        .then((res: resInfo) => {
+          console.log(res);
+          const { access_token } = res.data;
+          axios
+            .post(
+              `https://kapi.kakao.com/v2/user/me`,
+              {},
+              {
+                headers: {
+                  Authorization: `Bearer ${access_token}`,
+                  "Content-type":
+                    "application/x-www-form-urlencoded;charset=utf-8",
+                },
+              }
+            )
+            .then((res: resInfo) => {
+              console.log(res.data.kakao_account.profile);
+            });
+        })
+        .catch((Error: object) => {
+          console.log(Error);
+        });
     }
   }, [code]);
   return <div></div>;
